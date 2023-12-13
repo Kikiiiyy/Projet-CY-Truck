b64bb0825d6dac78c162727180d45dc0e274b85f
Killian <killian.harrois@gmail.com>
2023-12-13 19:49:16 +0100

    fonction recupData fini, ajout de traitementT.txt pour faire des tests

--- main.c
+++ main.c
@@ -1,51 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
-//test
+#include <string.h>
+
+#define MAX_LINE_LENGTH 100
+
 
 typedef struct dataT{
-    char nom;
+    char* nom;
     int nbVisite;
 }DataT;
 
-dataT creationT(){
-    DataT n=malloc(sizeof(DataT));
+DataT* creationT(){
+    DataT* n=malloc(sizeof(DataT));
     if(n==NULL){
-        printf("Erreur zebi");
+        printf("Erreur 1");
         exit(1);
     }
-    n.nom="";
-    n.nbVisite=0;
+    n->nom=(char*)malloc(sizeof(char)*100);
+    if(n->nom==NULL){
+    	printf("Erreur 2");
+    	exit(1);
+    }
+    n->nbVisite=0;
     return n;
 }
 
 
-dataD1 recupData(char villeRecherche){
+DataT* recupData(char* villeRecherche){
     FILE *fichier;
-    fichier = fopen("nom.txt", "r");
+    fichier = fopen("traitementT.txt", "r");
     if(fichier==NULL){
         printf("Erreur : impossible d ouvrir le fichier txt.");
-        return 0;
+        exit(1);
     }
-    DataT v=creationT();
+    DataT* v=creationT();
     char ligne[100];
     char villeA[50];
     char villeB[50];
     int idEtape;
 
-    while(fgets(ligne, sizeof(ligne), fichier) != NULL){
-        sscanf(ligne, "%[^;];%[^;];%d", villeA, villeB, &idEtape);
+    while(fgets(ligne, MAX_LINE_LENGTH, fichier) != NULL){
+    	/*/
+    	jsp pourquoi ca marche quand ya		villeA;villeB;idEtape
+    	mais pas
+    						idEtape;villeA,villeB
+    	/*/
+        sscanf(ligne, "%d;%49[^;];%49[^;\n]", &idEtape, villeA, villeB);
 
-        if(idEtape==1){
-            if(strcmp(villeRecherche, villeA)==0){
-                v.nbVisite++;
-            }
+        if(strcmp(villeRecherche, villeA)==0 && idEtape==1){
+             v->nbVisite++;
         }
         if(strcmp(villeRecherche, villeB)==0){
-            v.nbVisite++;
+            v->nbVisite++;
         }
     }
+    
+    
+    fclose(fichier);
     return v;
 }
+
 
 pArbre ajtArbre(){
     char* ville;
@@ -68,14 +82,9 @@ pArbre ajtArbre(){
 
 
 
-
-
-
-
-
-
-
-
 int main(){
-    return 0;
+	DataT* test=creationT();
+	test=recupData("CESTAS");
+	printf("%d\n", test->nbVisite);
+	return 0;
 }
